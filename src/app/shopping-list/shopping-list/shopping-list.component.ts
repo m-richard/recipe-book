d60104f4aa56e0ad087620c78ca8410de8bd63af
Marcus Richard <msrichard18@gmail.com>
2017-04-09 15:19:00 -0400

    made ingredients show up on page

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
-  
+  ingredients: Ingredient[] = [
+    new Ingredient('Potatoes', 5),
+    new Ingredient('Tomatoes', 9),
+  ];
+
   constructor() { }
 
   ngOnInit() {
